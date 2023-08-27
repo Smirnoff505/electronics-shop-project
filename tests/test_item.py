3b50a0f2cb0f693a4f03385444c991c320327cb6
Oleg Smirnov <oleg.smirnoff505@yandex.ru>
2023-08-27 20:09:55 +0500

    add tests

--- tests/test_item.py
+++ tests/test_item.py
@@ -18,3 +18,14 @@ def test_item1_pay_rate(item1):
 
 def test_item1_calculate_total_price(item1):
     assert item1.calculate_total_price() == 200000
+
+
+def test_string_to_number():
+    assert Item.string_to_number('5') == 5
+    assert Item.string_to_number('5.0') == 5
+    assert Item.string_to_number('5.5') == 5
+
+
+def test_instantiate_from_csv():
+    Item.instantiate_from_csv()
+    assert len(Item.all) == 5
