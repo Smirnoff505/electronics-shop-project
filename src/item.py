0c971eab8d3774237e9035fe93c94e1cf133f01a
Oleg Smirnov <oleg.smirnoff505@yandex.ru>
2023-08-27 20:09:33 +0500

    add @classmethod, @staticmethod, @property

--- src/item.py
+++ src/item.py
@@ -1,3 +1,7 @@
+import csv
+import os
+
+
 class Item:
     """
     Класс для представления товара в магазине.
@@ -13,7 +17,7 @@ class Item:
         :param price: Цена за единицу товара.
         :param quantity: Количество товара в магазине.
         """
-        self.name = name
+        self.__name = name
         self.price = price
         self.quantity = quantity
 
@@ -30,3 +34,36 @@ class Item:
         Применяет установленную скидку для конкретного товара.
         """
         self.price = self.pay_rate * self.price
+
+    @classmethod
+    def instantiate_from_csv(cls):
+        result = []
+        with open('../src/items.csv',
+                  newline='', encoding='windows-1251') as csvfile:
+            reader = csv.DictReader(csvfile)
+            for row in reader:
+                values = row['name'], float(row['price']), int(row['quantity'])
+                result.append(values)
+            cls.all = result
+
+    @staticmethod
+    def string_to_number(number_str: str):
+        num = []
+        for letter in number_str:
+            if letter.isdigit():
+                num.append(letter)
+            else:
+                break
+        number_int = int(''.join(num))
+        return number_int
+
+    @property
+    def name(self):
+        return self.__name
+
+    @name.setter
+    def name(self, product_name: str) -> None:
+        if len(product_name) > 10:
+            self.__name = product_name[:10]
+        else:
+            self.__name = product_name
